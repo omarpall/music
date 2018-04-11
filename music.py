37b9c9893c32c16f42f7b6e7c15b30d5c529d139
Stefán Páll Sturluson <stefansturlu@gmail.com>
2018-04-11 23:50:05 +0000

    Setti dæmi um hvernig maður les inn lag í music.py

--- music.py
+++ music.py
@@ -1,2 +1,15 @@
 import numpy as np
+from python_speech_features import mfcc
+from python_speech_features import delta
+from python_speech_features import logfbank
+import scipy.io.wavfile as wav
 
+
+# Dæmi um lestur fyrsta blues lagsins
+(rate,sig) = wav.read("waves/blues.00000.wav")
+mfcc_feat = mfcc(sig,rate)
+d_mfcc_feat = delta(mfcc_feat, 2)
+fbank_feat = logfbank(sig,rate)
+
+print(fbank_feat.shape)
+print(fbank_feat[1:3,:])
