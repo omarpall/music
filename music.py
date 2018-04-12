878650bbe7ef8f07b8246d747129227908d27251
Stefán Páll Sturluson <stefansturlu@gmail.com>
2018-04-12 11:10:36 +0000

    Les inn skrá og plotta hana upp

--- music.py
+++ music.py
@@ -3,10 +3,11 @@ from python_speech_features import mfcc
 from python_speech_features import delta
 from python_speech_features import logfbank
 import scipy.io.wavfile as wav
-
+import matplotlib.pyplot as plt
 
 # Dæmi um lestur fyrsta blues lagsins
-(rate,sig) = wav.read("waves/blues.00000.wav")
+(rate,sig) = wav.read("waves/blues.00001.wav")
+plawt = plt.plot(np.arange(sig.shape[0])/rate,sig)
 mfcc_feat = mfcc(sig,rate)
 d_mfcc_feat = delta(mfcc_feat, 2)
 fbank_feat = logfbank(sig,rate)
