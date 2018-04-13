34ef985626428a96f1ef6f41f973a3cb71410b18
Stefán Páll Sturluson <stefansturlu@gmail.com>
2018-04-13 18:13:12 +0000

    Hlé í bili. Prófum meira næst.

--- music.py
+++ music.py
@@ -7,13 +7,14 @@ from sklearn.model_selection import GridSearchCV
 #(rate,sig) = wav.read("waves/blues.00000.wav")
 #plt.plot(np.arange(sig.shape[0])/rate,sig)
 
-# Lesum inn gögnin og setjum mfcc features í fylki
+# Read the data (mfcc features, deltas and delta-deltas)
 MFCC = np.load("MFCC.npy")
 d_MFCC = np.load("d_MFCC.npy")
 dd_MFCC = np.load("dd_MFCC.npy")
 
 # Choose X
-X = np.concatenate((MFCC, d_MFCC, dd_MFCC), axis=1)
+X = MFCC
+#X = np.concatenate((MFCC, d_MFCC, dd_MFCC), axis=1)
 
 # Put the right labels in a vector y
 flokkar = ["blues","classical","country","disco","hiphop","jazz","metal","pop","reggae","rock"]
@@ -22,9 +23,11 @@ for i in range(10):
     y[100*i:100*(i+1)] = flokkar[i]
 
 
-# Split into training and testing set
+# Split into training, testing and validation set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=150, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=150, random_state=42)
+
 
 # Scale the data
 from sklearn.preprocessing import StandardScaler 
@@ -32,17 +35,30 @@ scaler = StandardScaler()
 scaler.fit(X_train)
 X_train_scaled = scaler.transform(X_train)
 X_test_scaled = scaler.transform(X_test)
+X_val_scaled = scaler.transform(X_val)
 
 #----------------- Classifiers ----------------------
 
-# Support Vector Machine 
+# Support Vector Machine - linear kernel
+C = [.0000001,.000001,.00001,.0001,.001,.01,1]
 
-parameters = {'kernel':('linear','rbf'), 'C':[0.1,1,10,100],'gamma':[.1,.01,.001]}
-svc = SVC()
-clf = GridSearchCV(svc, parameters)
-clf.fit(xTrain_scaled, yTrain)
-acc = clf.score(xTest_scaled,yTest)
-print("The accuracy is:",acc)
+# 100 random indexes from the train
+rand = np.random.choice(len(X_train),size=700,replace=False)
+
+best_C = 0
+best_acc = 0
+for c in C:
+    print("C:",c)
+    clf = SVC(C=c,kernel='linear') 
+    clf.fit(X_train_scaled[rand],y_train[rand])
+    acc = clf.score(X_val_scaled,y_val) 
+    print(acc)
+    if (acc > best_acc):
+        best_acc = acc
+        best_C = c
+print("The best cost value is C =",best_C)
+print("The best accuracy is",best_acc)
+
         
 # Confusion matrix
 from sklearn.metrics import confusion_matrix
