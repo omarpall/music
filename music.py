833f118f74dc66b47f9f86bfc299885a382a5cb4
Stefán Páll Sturluson <stefansturlu@gmail.com>
2018-04-13 15:04:50 +0000

    Bætti delta og deltadeltas

--- music.py
+++ music.py
@@ -1,5 +1,6 @@
 import numpy as np
 from python_speech_features import mfcc
+from python_speech_features import delta
 import scipy.io.wavfile as wav
 import matplotlib.pyplot as plt
 
@@ -9,8 +10,10 @@ plt.plot(np.arange(sig.shape[0])/rate,sig)
 
 # Lesum inn gögnin og setjum mfcc features í fylki
 X = np.load("MFCC.npy")
+# Setjum rétt labels í y vigur
 flokkar = ["blues","classical","country","disco","hiphop","jazz","metal","pop","reggae","rock"]
 y = np.array(['' for _ in range(1000)], dtype=object)
 for i in range(10):
     y[100*i:100*(i+1)] = flokkar[i]
+
 
--- saveFeatures.py
+++ saveFeatures.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from python_speech_features import mfcc
-#from python_speech_features import delta
+from python_speech_features import delta
 #from python_speech_features import logfbank
 import scipy.io.wavfile as wav
 
 MFCC = []
+d_MFCC = []
+dd_MFCC = []
 flokkar = ["blues","classical","country","disco","hiphop","jazz","metal","pop","reggae","rock"]
 for flokkur in flokkar:
     print(flokkur)
@@ -14,8 +16,16 @@ for flokkur in flokkar:
         (rate,sig) = wav.read(file)
         sig = sig[0:660000] # Minnst lagið er 660000 punktar
         mfcc_feat = mfcc(sig,rate,nfft=1024)
+        d_mfcc_feat = delta(mfcc_feat,2)
+        dd_mfcc_feat = delta(d_mfcc_feat,2)
         MFCC = np.append(MFCC, mfcc_feat.flatten())
+        d_MFCC = np.append(d_MFCC, d_mfcc_feat.flatten())
+        dd_MFCC = np.append(dd_MFCC, dd_mfcc_feat.flatten())
 MFCC = MFCC.reshape((1000,38805))        
+d_MFCC = d_MFCC.reshape((1000,38805))        
+dd_MFCC = dd_MFCC.reshape((1000,38805))        
 
 # Vista mfcc features sem MFCC.npy
 np.save("MFCC.npy", MFCC)
+np.save("d_MFCC.npy", d_MFCC)
+np.save("dd_MFCC.npy", dd_MFCC)
